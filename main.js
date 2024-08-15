2fdf7fec1480661b78088b7b5d9e1734536ad54a
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2024-08-15 12:37:40 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let myName = prompt('Ismingizni kiriting');
+let myId = prompt('Space idingizni kiriting');
+let myCode = prompt('Space psswordingizni kiriting');
+const acc = {
+    name: myName,
+    id: myId,
+    password: myCode
+}
+
+console.log(acc);
